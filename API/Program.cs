55fb107ea977960c6411eb91b7541cb6d89ff87b
Geoff George <geoffgeorgethommana@gmail.com>
2025-12-25 10:18:43 +0530

    re

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 var app = builder.Build();
 
 
-// Configure the HTTP request pipeline..
+// Configure the HTTP request pipeline...
 // if (app.Environment.IsDevelopment())
 // {
    
